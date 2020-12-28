e13907fa2afd7b56af57c8a42c23c8423c9bbc22
austinkloske22 <austinkloske@gmail.com>
2020-12-28 17:19:34 +0100

    initial commit

---
+++ contracts/TodoList.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    uint taskCount = 0;
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+} 
